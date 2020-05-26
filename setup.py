2ce9dadc622145d9e6418793c4325aaa0a04e2e4
Chris Moffitt <chris@moffitts.net>
2020-05-25 19:24:19 -0500

    Support markdown for long description

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='sidetable',
     name='sidetable',
