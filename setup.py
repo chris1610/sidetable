5bedaf0d02f1baff2fb8e218df481656da33e334
Chris Moffitt <chris@moffitts.net>
2020-05-25 19:32:16 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/chris1610/sidetable',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
--- sidetable/__init__.py
+++ sidetable/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Chris Moffitt"""
 __email__ = 'chris@moffitts.net'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .sidetable import SideTableAccessor
 
